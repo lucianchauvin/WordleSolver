9084c72851fe994a7a5bc836209f2216247203e9
Lucian Chauvin <lucianchauvin@gmail.com>
2022-02-19 00:45:01 -0600

    added lookup

--- Pair.java
+++ Pair.java
@@ -1,24 +1,24 @@
 public class Pair implements Comparable<Pair> {
-    String s;
-    Double i;
-  
-  public Pair(String s, Double i){
-      this.s = s;
-      this.i = i;
-  }
+  String s;
+  Double i;
 
-  public String getWord(){
-    return s;
-  }
+public Pair(String s, Double i){
+    this.s = s;
+    this.i = i;
+}
 
-  public Double getValue(){
-    return i;
-  }
+public String getWord(){
+  return s;
+}
 
-  public int compareTo(Pair pair) {
-      return (int) Math.round(this.i*1000) - ((int) Math.round(pair.getValue()*1000));
-    }
-  public String toString(){
-    return "(" + s + " " + i + ")";
+public Double getValue(){
+  return i;
+}
+
+public int compareTo(Pair pair) {
+    return (int) Math.round(this.i*1000) - ((int) Math.round(pair.getValue()*1000));
   }
+public String toString(){
+  return "(" + s + " " + i + ")";
+}
 }
